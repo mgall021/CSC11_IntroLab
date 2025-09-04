45f75cb160089ff63bc40845c78578f40754e290
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2025-09-03 19:06:22 -0700

    HelloWorld

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* 
+ * File:   main.cpp
+ * Author: Marco Gallegos
+ * Created on September 2, 2025, 3:30 PM
+ * Purpose:  Our First Program
+ *           Ritchie and Kerningham from the book The C Programming Language
+ *           Hello World Program
+ */
+#include <iostream>
+int main() {
+    std::cout << "Hello World!!\n";
+    return 0;
+}
